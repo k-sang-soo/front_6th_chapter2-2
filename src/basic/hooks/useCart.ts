8e9b7dcd48162e7b56be64367c01d333bf23ea1b
sangsoo <wsckm1@gmail.com>
2025-08-07 18:30:21 +0900

    feat(basic): cart 애플리케이션 로직

--- src/basic/hooks/useCart.ts
+++ src/basic/hooks/useCart.ts
@@ -24,6 +24,82 @@
 // - getRemainingStock: 재고 확인 함수
 // - clearCart: 장바구니 비우기 함수
 
+import { useLocalStorage } from '../utils/hooks/useLocalStorage.ts';
+import { useCallback, useEffect, useState } from 'react';
+import { ProductWithUI } from '../constants';
+import { addItemToCart, removeItemFromCart, updateCartItemQuantity } from '../models/cart.ts';
+import { CartItem, Coupon } from '../../types.ts';
+
 export function useCart() {
-  // TODO: 구현
+  const [cart, setCart] = useLocalStorage<CartItem[]>('cart', []);
+  const [selectedCoupon, setSelectedCoupon] = useState<Coupon | null>(null);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      localStorage.setItem('cart', JSON.stringify(cart));
+    } else {
+      localStorage.removeItem('cart');
+    }
+  }, [cart]);
+
+  const addToCart = useCallback(
+    (product: ProductWithUI) => {
+      const { success, error, cart: newCart } = addItemToCart(cart, product);
+
+      if (success) {
+        setCart(newCart!);
+        return { success };
+        // onAddNotification(result.success.message, 'success');
+      }
+
+      if (error) {
+        return { error };
+      }
+
+      // onAddNotification(result.error.message, 'error');
+    },
+    [cart, setCart],
+  );
+
+  const removeFromCart = (productId: string) => {
+    const { cart: newCart } = removeItemFromCart(cart, productId);
+    setCart(newCart);
+  };
+
+  const updateQuantity = (products: ProductWithUI[], productId: string, newQuantity: number) => {
+    const {
+      success,
+      error,
+      cart: newCart,
+    } = updateCartItemQuantity(cart, products, productId, newQuantity);
+
+    if (success) {
+      setCart(newCart!);
+      return { success };
+    }
+
+    if (error) {
+      return { error };
+      // onAddNotification(result.error.message, 'error');
+    }
+  };
+
+  const clearSelectedCoupon = useCallback(
+    (couponCode: string) => {
+      if (selectedCoupon?.code === couponCode) {
+        setSelectedCoupon(null);
+      }
+    },
+    [selectedCoupon],
+  );
+
+  return {
+    cart,
+    selectedCoupon,
+    addToCart,
+    removeFromCart,
+    updateQuantity,
+    clearCart: () => setCart([]),
+    clearSelectedCoupon,
+  };
 }
