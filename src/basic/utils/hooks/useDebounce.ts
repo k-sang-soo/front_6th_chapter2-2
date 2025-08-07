2ac4b7c130e029457d75756f60093de829a3478e
sangsoo <wsckm1@gmail.com>
2025-08-08 01:13:18 +0900

    feat(basic): useDebounce hook 구현 완료

--- src/basic/utils/hooks/useDebounce.ts
+++ src/basic/utils/hooks/useDebounce.ts
@@ -1,12 +1,17 @@
-// TODO: 디바운스 Hook
-// 힌트:
-// 1. 값이 변경되어도 지정된 시간 동안 대기
-// 2. 대기 시간 동안 값이 다시 변경되면 타이머 리셋
-// 3. 최종적으로 안정된 값만 반환
-//
-// 사용 예시: 검색어 입력 디바운싱
+import { useEffect, useState } from 'react';
 
-export function useDebounce<T>(_value: T, _delay: number): T {
-  // TODO: 구현
-  return _value; // 임시 반환값
+export function useDebounce<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
 }
